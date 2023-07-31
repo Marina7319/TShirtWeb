e7a833fa9c464f368356b14d0a381cec2f1ba1f9
Marina7319 <marina-markova@abv.bg>
2023-07-31 21:19:14 +0300

    Adding Models folder and commit Category.cs file in TshirtWebRazor project.

---
+++ Tshirt/TshirtWebRazor/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace TshirtWebRazor.Models
+{
+    using System.ComponentModel;
+
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+
+
+    }
+}
