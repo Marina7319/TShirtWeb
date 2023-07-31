4587e299843fc791932da7ddbb5bb86a83b62be1
Marina7319 <marina-markova@abv.bg>
2023-07-31 21:51:13 +0300

    Modify _Layout.cshtml file. Commite Idnex.cshtml file.

--- Tshirt/TshirtWebRazor/Pages/Categories/Index.cshtml.cs
+++ Tshirt/TshirtWebRazor/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TshirtWebRazor.Data;
+using TshirtWebRazor.Models;
 
 namespace TshirtWebRazor.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext database;
+        public List<Category> CategoryList {  get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            database = db;
+        }
         public void OnGet()
         {
+            CategoryList = database.Categories.ToList();
         }
     }
 }
