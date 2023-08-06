59d81f35b21cf5347fa8dcc8d27b2b9fc3ee7a1f
Marina7319 <marina-markova@abv.bg>
2023-08-06 16:13:37 +0300

    Delete ShoppingCartApi

--- Tshirt/T-shirt.ShoppingCart.Api/Models/Data/CartDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace T_shirt.ShoppingCart.Api.Models.Data
-{
-    public class CartDto
-    {
-
-        public CartHeaderDto CartHeader {  get; set; }
-        public IEnumerable<CartDetailsDto>? CartDetails { get; set; }
-    }
-}
