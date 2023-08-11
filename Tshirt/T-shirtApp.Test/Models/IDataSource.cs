f0af6f9efdbd7bcbb6bceba6fed3963d58ffc6d9
Marina7319 <marina-markova@abv.bg>
2023-08-11 12:39:50 +0300

    Commit
    Commit Project For Tests.

---
+++ Tshirt/T-shirtApp.Test/Models/IDataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T_shirt.Models.Models;
+
+namespace T_shirtApp.Test.Models
+{
+    public interface IDataSource
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
