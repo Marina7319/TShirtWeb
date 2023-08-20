b70ecf2cb9f029e6b53a467ca6faa0a9c9a0ccac
Marina7319 <marina-markova@abv.bg>
2023-08-20 09:03:33 +0300

    Commit.

---
+++ Tshirt/T-shirt.Models/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T_shirt.Models.Models;
+
+namespace T_shirt.Models.ViewModels
+{
+    public class CategoryViewModel
+    {
+
+        public IEnumerable<Category> Category { get; set; }
+
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
